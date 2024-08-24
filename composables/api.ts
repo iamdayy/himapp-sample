c50c37cd931c6a1df5fb062700e932b699f6e3d5
iamdayy <108299143+iamdayy@users.noreply.github.com>
2024-08-24 08:03:15 +0700

    check if not error

--- composables/api.ts
+++ composables/api.ts
@@ -2,15 +2,16 @@ export function $api<T>(
   request: Parameters<typeof $fetch<T>>[0],
   opts?: Parameters<typeof $fetch<T>>[1]
 ) {
+  const authState = useAuthState();
   const auth = useAuth();
 
-  if (!auth.token.value) {
+  if (!authState.token.value) {
     auth.refresh();
   }
   return $fetch<T>(request, {
     ...opts,
     headers: {
-      authorization: auth.token.value ? auth.token.value : "",
+      Authorization: auth.token.value ? auth.token.value : "",
       ...opts?.headers,
     },
   });
